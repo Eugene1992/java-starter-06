50d3b264b6bf4ede4f5bb1eaa0348525482dda9c
julia-zz <voldemar22>
2016-06-07 21:18:46 +0300

    Commint2.1

---
+++ yulia/src/main/java/NewClass.java
@@ -0,0 +1,8 @@
+/**
+ * Created by admin on 07.06.2016.
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        System.out.println("Hello from another class!!!");
+    }
+}
