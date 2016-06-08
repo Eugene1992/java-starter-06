207afe8a9a8b179edb88db05dab33e1a7342c924
Eugene <deyneko55@gmail.com>
2016-06-08 13:42:30 +0300

    hw01 and examples added

---
+++ src/examples/lesson_01/Test.java
@@ -0,0 +1,44 @@
+package lesson_01;
+
+public class Test {
+    public static void main(String[] args) {
+        /* boolean */
+        boolean isTrue = true;
+        boolean artem;
+
+        /* byte */
+        byte b;
+        b = 10;
+
+        byte b1, b2;
+        byte b3 = 10, b4 = 15;
+
+        /* short */
+        short s = 180;
+        short s5 = 10;
+
+        short s1 = (short)(s / 10);
+        short s2 = (short)(s * s5);
+
+        short s3 = 180 + 10;
+        short s4 = 180 - 10;
+
+        int i = 17;
+        int i1 = 3;
+        int result = i % i1 / 2;
+
+        int k = 7 / 2;
+
+        double d = 7 / 2.0;
+
+        float f = (float)2.0;
+        float f1 = 2.0f;
+
+        char c = 125;
+        char c1 = 'c';
+
+        long l = 1005353343435364434L;
+
+        System.out.println(l);
+    }
+}
