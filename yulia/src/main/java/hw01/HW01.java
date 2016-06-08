e92566595f913352a894d0397c2582b8e9c11ad4
julia-zz <voldemar22>
2016-06-08 20:31:41 +0300

    Commint2.1.2

--- yulia/src/main/java/hw01/HW01.java
+++ yulia/src/main/java/hw01/HW01.java
@@ -78,12 +78,15 @@ public class HW01 {
 
         /* Задачка */
 
-        int g = 16;
+        int g = 127;
         int g0;
         int g1;
         int g2;
         int g3;
         int g4;
+        int g5;
+        int g6;
+        int g7;
 
         g0 = g % 2;
         g = g / 2;
@@ -95,8 +98,15 @@ public class HW01 {
         g = g / 2;
         g4 = g % 2;
         g = g / 2;
+        g5 = g % 2;
+        g = g / 2;
+        g6 = g % 2;
+        g = g / 2;
+        g7 = g % 2;
+        g = g / 2;
 
-        System.out.println(g4+""+g3+""+g2+""+g1+""+g0);
+
+        System.out.println(g7+""+g6+""+g5+""+g4+""+g3+""+g2+""+g1+""+g0);
 
     }
 }
